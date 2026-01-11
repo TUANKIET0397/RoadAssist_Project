424fba6778c76afa1c97e7ecb1b949609f0ec9a9
Billy3005 <giabao3052005@gmail.com>
2026-01-11 12:07:19 +0700

    delete main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:road_assist/app.dart';
 
-// import 'package:road_assist/presentation/views/chat/chat_screen.dart';
-// import 'package:road_assist/presentation/viewmodels/chat/chat_viewmodel.dart';
-// import 'package:provider/provider.dart';
 
 // //firebase
 import 'package:firebase_core/firebase_core.dart';
@@ -25,18 +22,3 @@ void main() async {
     ),
   );
 }
-
-// void main() {
-//   runApp(
-//     MultiProvider(
-//       providers: [
-//         // Khai báo Provider cho ChatViewModel ở đây
-//         ChangeNotifierProvider(create: (_) => ChatViewModel()),
-//       ],
-//       child: const MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: ChatScreen(),
-//       ),
-//     ),
-//   );
-// }
